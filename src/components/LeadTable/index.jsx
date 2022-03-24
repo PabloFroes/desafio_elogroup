9ba75551b920d5315913be0505b33dbfb64308a3
Pablo Froes <pablofroes@outlook.com>
2022-03-24 03:43:44 -0300

    Implemented Drag and Drop

--- src/components/LeadTable/index.jsx
+++ src/components/LeadTable/index.jsx
@@ -7,17 +7,54 @@ import "./leadTable.css"
 export function LeadTable () {
 
     const [leads, setLeads] = useState([])
+    const [index,setIndex] = useState(0)
+    const [indexTarget, setIndexTarget] = useState(0)
+    const [row,setRow] = useState(0)
+    const [rowTarget, setRowTarget] = useState(0)
+    const status = ["Cliente em Potencial","Dados Confirmados","Reunião Agendada"]
 
+    async function fetchLeads () {
+        const response = await LeadService.get();
+        if(response !== null){
+            setLeads(response)
+        }
+    }
     
     useEffect(() => {
-        async function fetchLeads () {
-            const response = await LeadService.get();
-            if(response !== null){
-                setLeads(response)
-            }
-        }
         fetchLeads();
     },[]);
+
+    function sameRow (event) {
+        if(row === rowTarget){
+            return true
+        }else {
+            return false
+        }
+    }
+
+    function drag (event,item,index) {
+        setIndex(index)
+        event.dataTransfer.setData("text", item.name);
+        setRow(event.target.parentElement.parentElement.id);
+    }
+
+
+    function dragEnd (event){
+        if(sameRow(event) && index < indexTarget){
+            event.target.innerHTML = ""       
+        }
+    }
+
+    function drop (event,item,status) {
+            if(index < indexTarget && sameRow()){
+                event.target.children[0].innerHTML= item.name
+            }
+    }
+    function dragOver (event, indexTarget){
+        setRowTarget(event.target.parentElement.id);
+        event.preventDefault();
+        setIndexTarget(indexTarget);
+    }
 
     return(
         <div className="container">
@@ -34,10 +71,10 @@ export function LeadTable () {
                 </thead>
                 <tbody>
                     {leads.map((item,key) =>
-                        <tr key={key} className="trLead">
-                            <td className="tdLead">{item.Status === "Cliente em Potencial" ? item.name : ""}</td>
-                            <td className="tdLead">{item.Status === "Dados Confirmados" ? item.name : ""}</td>
-                            <td className="tdLead">{item.Status === "Reunião Agendada" ? item.name : ""}</td>
+                        <tr id={key} key={key} className="trLead">
+                            <td className="tdLead" onDragOver={event => dragOver(event, 0)} onDrop={event => drop(event, item, status[0])}><p draggable="true" onDragStart={event => drag(event,item,0)} onDragEnd={event => dragEnd(event)}> {item.Status === status[0] ? item.name : ""}</p></td>
+                            <td className="tdLead" onDragOver={event => dragOver(event, 1)} onDrop={event => drop(event, item, status[1])}><p draggable="true" onDragStart={event => drag(event,item,1)} onDragEnd={event => dragEnd(event)} > {item.Status === status[1] ? item.name : " "}</p></td>
+                            <td className="tdLead" onDragOver={event => dragOver(event, 2)} onDrop={event => drop(event, item, status[2])}><p draggable="false" onDragEnd={event => dragEnd(event)} > {item.Status === status[2] ? item.name : " "}</p></td>
                         </tr>
                     )}
                 </tbody>
